a4c5b875471971c1f81c627fd9d06d4eab140153
binodtamang24 <67016236+B-nod@users.noreply.github.com>
2024-11-01 22:03:13 +0545

    'productandcategory'

--- controllers/productController.js
+++ controllers/productController.js
@@ -8,7 +8,7 @@ exports.postProduct = async(req,res)=>{
         product_price: req.body.product_price,
         countInStock: req.body.countInStock,
         product_description:req.body.product_description,
-        product_image:req.body.product_image,
+        product_image:req.file.path,
         category:req.body.category,
     })
     product = await product.save()
@@ -53,7 +53,7 @@ exports.updateProduct = async (req, res) => {
             product_price: req.body.product_price,
             countInStock: req.body.countInStock,
             product_description: req.body.product_description,
-            product_image: req.body.product_image,
+            product_image: req.file.paths,
             category: req.body.category,
         }, { new: true });
 
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const { postProduct, productlist, productDetails, updateProduct, deleteProduct } = require("../controllers/productController")
 const router = express.Router()
+const upload = require('../middleware/fileUpload')
 
-router.post('/postproduct', postProduct)
+router.post('/postproduct',upload.single('product_image'), postProduct)
 router.get('/productlist', productlist)
 router.get('/productdetails/:id', productDetails),
-router.put('/updateproduct/:id', updateProduct),
+router.put('/updateproduct/:id', upload.single('product_image'), updateProduct),
 router.delete('/deleteproduct/:id', deleteProduct)
 
 
