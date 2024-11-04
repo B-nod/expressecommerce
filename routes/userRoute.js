532d98899a04cf6a95d03f3f15644cf4ae0edca7
binodtamang24 <67016236+B-nod@users.noreply.github.com>
2024-11-04 11:08:10 +0545

    'auth'

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,15 @@
 const express = require('express')
-const { postUser, postEmailConfirmation, signIn } = require('../controllers/userController')
+const { postUser, postEmailConfirmation, signIn, forgetPassword, resetPassword, userList, userDetials, signOut, requireSignin } = require('../controllers/userController')
 const router = express.Router()
 
 router.post('/register', postUser)
 router.put('/confirmation/:token', postEmailConfirmation)
 router.post('/signin', signIn)
+router.post('/forgetpassword', forgetPassword)
+router.put('/resetpassword/:token', resetPassword)
+router.get('/userlist', requireSignin, userList)
+router.get('/userdetails/:id',requireSignin, userDetials)
+router.post('/signout', signOut)
 
 
 module.exports = router
