b79e3a9ba1cadd51569c777e4bca3564d63e7430
binodtamang24 <67016236+B-nod@users.noreply.github.com>
2024-11-04 11:10:27 +0545

    'authorization'

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,11 +3,12 @@ const { postProduct, productlist, productDetails, updateProduct, deleteProduct }
 const router = express.Router()
 const upload = require('../middleware/fileUpload')
 const{productValidation, validation}=require('../validation/validator')
+const { requireSignin, requireAdmin } = require("../controllers/userController")
 
-router.post('/postproduct',upload.single('product_image'), productValidation, validation, postProduct)
+router.post('/postproduct', requireSignin, requireAdmin,upload.single('product_image'), productValidation, validation, postProduct)
 router.get('/productlist', productlist)
-router.get('/productdetails/:id', productDetails),
-router.put('/updateproduct/:id', upload.single('product_image'), productValidation, validation,  updateProduct),
+router.get('/productdetails/:id', requireSignin, requireAdmin, productDetails),
+router.put('/updateproduct/:id', requireSignin, requireAdmin, upload.single('product_image'), productValidation, validation,  updateProduct),
 router.delete('/deleteproduct/:id', deleteProduct)
 
 
